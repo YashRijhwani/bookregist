eae5128f994cd0fd993057b8e9b08b1a0649ed1f
samthatcode <samthatcode@gmail.com>
2024-03-28 10:58:41 +0100

    Ran the build command

--- server/server.js
+++ server/server.js
@@ -85,7 +85,7 @@ app.use('/api/v1', apiRoute);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get('/term-of-use', (req, res) => {
